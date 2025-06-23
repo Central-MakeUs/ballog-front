73612ab2fcf3b3e619fa81a99095a03c4e16146c
kimyounghee425 <doctorsean0425@gmail.com>
2025-06-23 17:00:21 +0900

    [chore] web 셋팅

---
+++ apps/web/src/constants/path.ts
@@ -0,0 +1,5 @@
+export const PATH = {
+    HOME : "/",
+    TEST : "/test",
+    TESTID: (id: string) => `/test/${id}`,
+}
---
+++ apps/web/src/router/AppRouter.tsx
@@ -0,0 +1,28 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import App from '../App'
+import HomePage from '../pages/homePage'
+import TestPage from '../pages/testPage'
+import TestId from '../pages/testId'
+
+import { PATH } from '../constants/path'
+
+const AppRouter = () => {
+  const router = createBrowserRouter([
+    {
+      path: PATH.HOME,
+      element: <App />,
+      children: [
+        { path: '', element: <HomePage /> },
+        { path: PATH.TEST, element: <TestPage /> },
+        {
+          path: 'test/:id',
+          element: <TestId />,
+        },
+      ],
+    },
+  ])
+
+  return <RouterProvider router={router} />
+}
+
+export default AppRouter
