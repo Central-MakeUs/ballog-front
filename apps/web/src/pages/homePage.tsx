9cf47adec5e097bf853931345d194ea8d2d9c2d6
kimyounghee425 <doctorsean0425@gmail.com>
2025-06-25 16:52:09 +0900

    ✨ [feat] shadcn 추가

--- apps/web/src/pages/homePage.tsx
+++ apps/web/src/pages/homePage.tsx
@@ -1,5 +1,12 @@
+import { Button } from '@/components/ui/button'
+
 const HomePage = () => {
-    return <h1 className="text-2xl bg-red-100">홈페이지</h1>
+  return (
+    <>
+      <h1 className="text-2xl bg-red-100">홈페이지</h1>
+      <Button>버튼</Button>
+    </>
+  )
 }
 
 export default HomePage
