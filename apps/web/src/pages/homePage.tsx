20423f2bb040da44922ff92f6e12fb29f69cdcf0
kimyounghee425 <doctorsean0425@gmail.com>
2025-06-27 17:58:38 +0900

    ✨ GNB 생성

--- apps/web/src/pages/homePage.tsx
+++ apps/web/src/pages/homePage.tsx
@@ -1,10 +1,12 @@
 import { Button } from '@/components/ui/button'
+import GlobalNavigationBar from '@/components/common/GlobalNavigationBar'
 
 const HomePage = () => {
   return (
     <>
       <h1 className="text-2xl bg-red-100">홈페이지</h1>
       <Button>버튼</Button>
+      <GlobalNavigationBar />
     </>
   )
 }
