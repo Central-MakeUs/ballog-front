581de303dbe06017c75167c59befab58598e7e45
kimyounghee425 <doctorsean0425@gmail.com>
2025-06-27 21:45:36 +0900

    ✨ [feat] 기록 종료 모달 생성

--- apps/web/src/pages/liveRecordPage.tsx
+++ apps/web/src/pages/liveRecordPage.tsx
@@ -1,13 +1,17 @@
 import Header from '@/components/common/Header'
 import GameInfoCard from '@/features/record/components/gameInfoCard'
 import EmotionRecorder from '@/features/record/components/emotionRecorder'
+import RecordEndModal from '@/features/record/components/recordEndModal'
 
 const LiveRecordPage = () => {
   return (
-    <div>
+    <div className="min-h-screen flex flex-col">
       <Header title={<p className="font-bold text-base">감정 기록 중</p>} />
-      <GameInfoCard />
-      <EmotionRecorder />
+      <div className="flex-grow flex-col items-center gap-4">
+        <GameInfoCard />
+        <EmotionRecorder />
+      </div>
+      <RecordEndModal />
     </div>
   )
 }
