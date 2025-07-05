20163c47e85c12e963ef564fb03bf1b86c973d30
YoungCheon <kyt031522@gmail.com>
2025-07-05 23:49:48 +0900

    ✨ Ky 세팅 (ky error intercept 해서 ExtendedKyHttpError 사용)

--- apps/web/src/types/api/common.ts
+++ apps/web/src/types/api/common.ts
@@ -1,10 +1,22 @@
+import type { HTTPError } from 'ky'
+
 export interface ApiResponse<T> {
   data: T
   statusCode: number
   message: string
 }
 
-export interface ApiError {
-  code: string
+export interface ApiErrorMessage {
+  error: string
   message: string
+  status: number
+  code: string
+}
+
+// ky 커스텀 에러 타입
+export type KyHttpError = HTTPError<ApiErrorMessage>
+
+// client에서 사용할 에러 타입
+export type ExtendedKyHttpError = KyHttpError & {
+  errorData: ApiErrorMessage
 }
