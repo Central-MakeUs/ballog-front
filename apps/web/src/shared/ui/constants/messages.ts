a7b97b21f495e1ff9b6dccf83ffb274e157b8881
YoungCheon <kyt031522@gmail.com>
2025-07-06 16:44:42 +0900

    ♻️ 상수 분리

---
+++ apps/web/src/shared/ui/constants/messages.ts
@@ -0,0 +1,9 @@
+export const AUTH_MESSAGES = {
+  nickname: {
+    placeholder: '닉네임',
+    lengthRule: '1자~10자까지 입력 가능합니다.',
+    regexRule: '한글, 영문, 숫자만 입력 가능합니다.',
+    duplicateRule: '이미 존재하는 닉네임입니다.',
+    required: '닉네임을 입력해주세요.',
+  },
+}
