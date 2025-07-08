3e9b61253f7a8330b15c2927b842c9eeee36ec27
kimyounghee425 <doctorsean0425@gmail.com>
2025-07-08 20:32:30 +0900

    ♻️ [refactor] RecordLogCard 합성컴포넌트 작업

--- apps/web/src/stories/Card/RecordLogCard.stories.tsx
+++ apps/web/src/stories/Card/RecordLogCard.stories.tsx
@@ -1,73 +1,41 @@
 import type { Meta, StoryObj } from '@storybook/react-vite'
 import { RecordLogCard } from '@/shared/ui/common/Card/RecordLogCard'
+import { Button } from '@/shared/ui/common'
 
-const meta: Meta<typeof RecordLogCard.WithRecord> = {
+const meta: Meta<typeof RecordLogCard.Root> = {
   title: 'Components/Card/RecordLogCard',
-  component: RecordLogCard.WithRecord,
+  component: RecordLogCard.Root,
   tags: ['autodocs'],
-  argTypes: {
-    homeTeam: {
-      control: 'text',
-      description: '홈 팀 이름',
-      defaultValue: 'LG 트윈스',
-    },
-    awayTeam: {
-      control: 'text',
-      description: '원정 팀 이름',
-      defaultValue: 'SSG 랜더스',
-    },
-    stadium: {
-      control: 'text',
-      description: '경기장 이름',
-      defaultValue: '잠실야구장',
-    },
-    date: {
-      control: 'text',
-      description: '경기 일시',
-      defaultValue: '2025.07.09 18:30',
-    },
-    matchesResult: {
-      control: { type: 'radio' },
-      options: ['win', 'lose', 'draw'],
-      description: '경기 결과',
-      defaultValue: 'lose',
-    },
-  },
 }
 
 export default meta
 type Story = StoryObj
 
-export const Test: Story = {
-  name: 'test',
-  args: {
-    homeTeam: 'LG 트윈스',
-    awayTeam: 'SSG 랜더스',
-    stadium: '잠실야구장',
-    date: '2025.07.09 (수) 오후 6:30',
-    matchesResult: 'lose',
-  },
-}
-
 export const WithRecord: Story = {
   render: () => (
-    <div style={{ height: '200px' }}>
-      <RecordLogCard.WithRecord
+    <RecordLogCard.Root>
+      <RecordLogCard.Info
         homeTeam="LG 트윈스"
         awayTeam="SSG 랜더스"
         stadium="잠실야구장"
-        date="2025.07.09 18:30"
-        matchesResult="lose"
+        date="2025.07.09 (수) 오후 6:30"
       />
-    </div>
+      <RecordLogCard.Badge result="win" />
+      <RecordLogCard.Footer onClick={() => alert('클릭')}>
+        경기 결과 보러가기
+      </RecordLogCard.Footer>
+    </RecordLogCard.Root>
   ),
+  name: 'WithRecord',
 }
 
 export const NoRecord: Story = {
   render: () => (
-    <div style={{ height: '200px' }}>
-      {' '}
-      <RecordLogCard.NoRecord />
-    </div>
+    <RecordLogCard.Empty>
+      <Button className="rounded-large px-6 bg-brand-secondary-default">
+        첫 직관 기록하기
+      </Button>
+    </RecordLogCard.Empty>
   ),
+  name: 'NoRecord',
 }
