d2ee7d7cf2b1ed488fc3790660dc257833bb6e65
kimyounghee425 <doctorsean0425@gmail.com>
2025-07-14 14:04:12 +0900

    ✨ [feat] 기록중 페이지에 RecordingCard 사용

--- apps/web/src/pages/home/ui/LiveRecordPage.tsx
+++ apps/web/src/pages/home/ui/LiveRecordPage.tsx
@@ -3,13 +3,23 @@ import GameInfoCard from '@/features/record/components/gameInfoCard'
 import EmotionRecorder from '@/features/record/components/emotionRecorder'
 import RecordEndModal from '@/features/record/components/recordEndModal'
 import type { ActivityComponentType } from '@stackflow/react'
+import { RecordingCard } from '@/shared/ui/common/Card/RecordingCard'
 
 const LiveRecordPage: ActivityComponentType = () => {
   return (
     <AppScreen appBar={{ title: '감정 기록 중' }}>
-      <div className="max-h-full flex flex-col">
+      <div className="max-h-full flex flex-col px-4">
+        <RecordingCard.Root className='w-full'>
+          <RecordingCard.Icon state="active" />
+          <RecordingCard.Info
+            homeTeam="LG 트윈스"
+            awayTeam="SSG 랜더스"
+            stadium="잠실야구장"
+            date="2025.07.09"
+          />
+        </RecordingCard.Root>
         <div className="flex-grow flex-col items-center gap-4">
-          <GameInfoCard />
+          {/* <GameInfoCard /> */}
           <EmotionRecorder />
         </div>
         <RecordEndModal />
