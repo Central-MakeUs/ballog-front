264f16495d9e2dc9617f09130adf6f3c64c944c0
YoungCheon <kyt031522@gmail.com>
2025-07-18 20:42:52 +0900

    ✏️ QueryProvider children type 수정
    너무 제한적이라 다른곳에 사용이 안되어서 수정

--- apps/web/src/app/Provider/QueryProvider.tsx
+++ apps/web/src/app/Provider/QueryProvider.tsx
@@ -6,7 +6,7 @@ const queryClient = new QueryClient()
 export default function QueryProvider({
   children,
 }: {
-  children: React.JSX.Element
+  children: React.ReactElement
 }) {
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
