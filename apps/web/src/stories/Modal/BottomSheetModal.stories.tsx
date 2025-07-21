ee100734718a6c2de62625a889d08ef7cc9af0ff
YoungCheon <kyt031522@gmail.com>
2025-07-21 12:24:47 +0900

    🚨 Lint 에러 해결

--- apps/web/src/stories/Modal/BottomSheetModal.stories.tsx
+++ apps/web/src/stories/Modal/BottomSheetModal.stories.tsx
@@ -16,22 +16,30 @@ type Story = StoryObj
 
 export const Default: Story = {
   render: () => {
-    const [open, setOpen] = useState(true)
+    const BottomSheetModalComponent = () => {
+      const [open, setOpen] = useState(true)
 
-    return (
-      <BottomSheetModal.Root open={open} onOpenChange={setOpen} dismissible={false}>
-        <BottomSheetModal.Text
-          heading="사진 업로드 완료"
-          body="업로드가 성공적으로 완료되었습니다."
-        />
-        <BottomSheetModal.Image src={grayExampleImage} />
-        <BottomSheetModal.Buttons
-          buttons={[
-            { label: '취소', onClick: () => setOpen(false) },
-            { label: '확인', onClick: () => alert('확인') },
-          ]}
-        />
-      </BottomSheetModal.Root>
-    )
+      return (
+        <BottomSheetModal.Root
+          open={open}
+          onOpenChange={setOpen}
+          dismissible={false}
+        >
+          <BottomSheetModal.Text
+            heading="사진 업로드 완료"
+            body="업로드가 성공적으로 완료되었습니다."
+          />
+          <BottomSheetModal.Image src={grayExampleImage} />
+          <BottomSheetModal.Buttons
+            buttons={[
+              { label: '취소', onClick: () => setOpen(false) },
+              { label: '확인', onClick: () => alert('확인') },
+            ]}
+          />
+        </BottomSheetModal.Root>
+      )
+    }
+
+    return <BottomSheetModalComponent />
   },
 }
