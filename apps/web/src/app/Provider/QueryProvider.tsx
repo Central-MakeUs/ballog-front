492d246ad2280b3917d19a377269b3f676f98e06
kimyounghee425 <doctorsean0425@gmail.com>
2025-07-22 17:12:20 +0900

    🔀 Merge dev

--- apps/web/src/app/Provider/QueryProvider.tsx
+++ apps/web/src/app/Provider/QueryProvider.tsx
@@ -1,5 +1,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import type { JSX } from 'react'
+
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 const queryClient = new QueryClient()
