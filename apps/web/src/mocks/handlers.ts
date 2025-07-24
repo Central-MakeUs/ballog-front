1a9d900d0a4c63af9cc6f95e56b64a837ec8ddba
YoungCheon <kyt031522@gmail.com>
2025-07-24 19:17:46 +0900

    🤡 Image mock handler 추가

--- apps/web/src/mocks/handlers.ts
+++ apps/web/src/mocks/handlers.ts
@@ -1,5 +1,11 @@
 import { authHandlers } from './handlers/authHandlers'
 import { matchHandlers } from './handlers/matchHandlers'
 import { recordHandlers } from './handlers/recordHandler'
+import { imageHandlers } from './handlers/imageHandler'
 
-export const handlers = [...authHandlers, ...matchHandlers, ...recordHandlers]
+export const handlers = [
+  ...authHandlers,
+  ...matchHandlers,
+  ...recordHandlers,
+  ...imageHandlers,
+]
