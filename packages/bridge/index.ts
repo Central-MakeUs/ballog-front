4fff5836496b646376a947a4edba099caf8882c6
YoungCheon <kyt031522@gmail.com>
2025-07-26 20:38:00 +0900

    feat: 앱 설정 및 이미지 핸들러 브릿지 개선

--- packages/bridge/index.ts
+++ packages/bridge/index.ts
@@ -14,4 +14,4 @@ export { createAppBridge } from './app'
 // React Web Bridge
 export { createWebBridge } from './web'
 
-export { POST_MESSAGE_EVENT } from './constants/postMessage'
+export * from './constants/postMessage'
