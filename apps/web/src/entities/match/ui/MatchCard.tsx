cdc0b3046821ac75238db2f688938a97bdd25a18
YoungCheon <kyt031522@gmail.com>
2025-08-02 14:20:22 +0900

    ✨ Mainpage bridge 처리ㅣ

--- apps/web/src/entities/match/ui/MatchCard.tsx
+++ apps/web/src/entities/match/ui/MatchCard.tsx
@@ -1,6 +1,7 @@
 import { HomeCard } from '@/shared/ui/common/Card/HomeCard'
 import { Button } from '@/shared/ui/common/Button'
 import { type Match } from '@/entities/match/model/match.type'
+import { useFlow } from '@/shared/lib/stackflow'
 
 type MatchCardProps = Match
 
@@ -10,7 +11,9 @@ export const MatchCard = ({
   stadium,
   matchesDate,
   matchesTime,
+  matchesId,
 }: MatchCardProps) => {
+  const { push } = useFlow()
   return (
     <HomeCard.Root>
       <HomeCard.MatchInfo homeTeam={homeTeam} awayTeam={awayTeam} />
@@ -18,7 +21,11 @@ export const MatchCard = ({
         stadium={stadium}
         dateTime={`${matchesDate} ${matchesTime}`}
       >
-        <Button>기록 시작하기</Button>
+        <Button
+          onClick={() => push('LiveRecord', { recordId: String(matchesId) })}
+        >
+          기록 시작하기
+        </Button>
       </HomeCard.DetailInfo>
     </HomeCard.Root>
   )
