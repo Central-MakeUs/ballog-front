2b2c88e7e4fc03eef9fa27f2c06fd823f423dcd4
YoungCheon <kyt031522@gmail.com>
2025-08-08 16:13:45 +0900

    ✏️ Auth patch dto 생성

--- apps/web/src/entities/auth/model/auth.type.ts
+++ apps/web/src/entities/auth/model/auth.type.ts
@@ -1,6 +1,11 @@
 import type { ApiResponse } from '@/types/api/common'
 import type { ApiResponseWithNoSuccess } from '@/types/api/common'
 
+export interface ChangeNicknameRequestDTO {
+  baseballTeam: string
+  nickname: string
+}
+
 export interface SignupRequestDTO {
   baseballTeam: string
   nickname: string
