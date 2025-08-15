cc478ab9a119f6cd5ee74a389dd0e6676da7c891
kimyounghee425 <doctorsean0425@gmail.com>
2025-08-15 20:58:30 +0900

    🐛 Fix

--- apps/web/src/mocks/handlers/recordHandler.ts
+++ apps/web/src/mocks/handlers/recordHandler.ts
@@ -17,8 +17,7 @@ export const recordHandlers = [
       // 네트워크 지연 효과 추가
       delay(1000)
 
-      // const isEmpty = Math.random() > 0.7
-      const isEmpty = 0
+      const isEmpty = Math.random() > 0.7
 
       if (isEmpty) {
         return HttpResponse.json<RecordResponseDTO>({
