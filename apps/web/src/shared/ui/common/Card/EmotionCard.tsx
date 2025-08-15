2464d79d83448bd25a6dee2635c518ac0170b326
kimyounghee425 <doctorsean0425@gmail.com>
2025-08-15 16:17:01 +0900

    💄 [style] 갭 추가

--- apps/web/src/shared/ui/common/Card/EmotionCard.tsx
+++ apps/web/src/shared/ui/common/Card/EmotionCard.tsx
@@ -140,7 +140,7 @@ const Active = ({
             'bg-usage-background-strong',
           )}
         >
-          <JoyEmotion className="w-5 h-5" />
+          <JoyEmotion className="w-5 h-5 mr-1" />
           <span>{centerEmotion}</span>
         </div>
       )}
