96cd2769ef5ff26b1fae0818d3d95d4294764788
YoungCheon <kyt031522@gmail.com>
2025-08-21 01:08:50 +0900

    ✏️ 타입 오류 해결 & 결과 null 처리

--- apps/web/src/entities/record/ui/RecordLogCard/Info.tsx
+++ apps/web/src/entities/record/ui/RecordLogCard/Info.tsx
@@ -3,13 +3,14 @@ import type { ComponentProps } from 'react'
 import { cn } from '@/shared/lib/classnames'
 import { TEAMS, type TeamKey } from '@/shared/constants/teams'
 import { getStadiumName, type StadiumKey } from '@/shared/constants/stadium'
+import type { RecordResult } from '@/entities/record/model/record.type'
 
 interface InfoProps extends ComponentProps<'div'> {
   homeTeam: TeamKey
   awayTeam: TeamKey
   stadium: StadiumKey
   date: string
-  result: 'WIN' | 'LOSE' | 'DRAW'
+  result: RecordResult
 }
 
 export const Info = ({
@@ -21,6 +22,8 @@ export const Info = ({
   className,
   ...rest
 }: InfoProps) => {
+  // 경기 결과가 없으면 일단 DRAW처리
+  // 하지만 렌더링 되지는 않음
   const fontColor = {
     WIN: 'text-brand-green-default',
     LOSE: 'text-brand-red-default',
@@ -38,7 +41,7 @@ export const Info = ({
       className={cn('relative p-6 gap-1 flex flex-col', className)}
       {...rest}
     >
-      <p className={cn('body-sm-bold', fontColor)}>{badgeText}</p>
+      {result && <p className={cn('body-sm-bold', fontColor)}>{badgeText}</p>}
       <p className="body-lg-bold text-usage-text-default">
         {TEAMS[homeTeam]} <span className="mx-2">vs</span> {TEAMS[awayTeam]}
       </p>
