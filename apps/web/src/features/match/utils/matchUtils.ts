cbe8e07eaec977b3f3bdcbbfa85ee3e022051026
kimyounghee425 <doctorsean0425@gmail.com>
2025-10-22 01:39:21 +0900

    ♻️ [refactor] 날짜 문자열비교 -> date 객체끼리 비교 수정

--- apps/web/src/features/match/utils/matchUtils.ts
+++ apps/web/src/features/match/utils/matchUtils.ts
@@ -1,5 +1,9 @@
-export const formatDate = (d: Date) =>
-  d
-    .toLocaleDateString('ko-KR', { timeZone: 'Asia/Seoul' })
-    .replace(/\.\s/g, '-')
-    .replace(/\.$/, '')
+import { toZonedTime } from 'date-fns-tz'
+
+import { TIME_ZONE } from '@/shared/constants/time'
+
+export const formatDate = (date: Date) => {
+  const zonedTime = toZonedTime(date, TIME_ZONE)
+  zonedTime.setHours(0, 0, 0, 0)
+  return zonedTime
+}
