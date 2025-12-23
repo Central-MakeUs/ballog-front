40be4b3b78b6f4f039f908da76c50a2f3466e4f8
kych0912 <kyt031522@email.com>
2025-12-23 20:43:18 +0900

    로그제거 & import goruf

--- apps/web/src/features/record/ui/EmotionDistribution/index.tsx
+++ apps/web/src/features/record/ui/EmotionDistribution/index.tsx
@@ -1,53 +1,8 @@
+import React from 'react'
+
 import { TeamEmotionDistribution } from '@/entities/record/ui/TeamEmotionDistribution'
 import { TeamsEmotionStat } from '@/entities/record/ui/TeamsEmotionStat'
 import { useGetRecordDetail } from '@/features/record/hooks/useGetRecordDetail'
-
-export const EmotionDistribution = ({
-  matchRecordId,
-}: {
-  matchRecordId: number
-}) => {
-  const { isUserSupportingTeam, isDuringMatch, recordDetail } =
-    useGetRecordDetail({
-      matchRecordId: matchRecordId,
-    })
-
-  if (!recordDetail) return <></>
-
-  const { homeTeam, awayTeam, positiveEmotionPercent, negativeEmotionPercent, emotionGroupList } =
-    recordDetail
-
-  if (isDuringMatch) {
-    return (
-      <>
-        <SectionContainer>
-          <TeamEmotionDistribution.Empty />
-        </SectionContainer>
-      </>
-    )
-  }
-
-  if(!isDuringMatch) {
-    return (
-        <SectionContainer>
-          <TeamsEmotionStat
-            homeTeamKey={homeTeam}
-            awayTeamKey={awayTeam}
-            homePositive={positiveEmotionPercent}
-            homeNegative={negativeEmotionPercent}
-            awayPositive={positiveEmotionPercent}
-            awayNegative={negativeEmotionPercent}
-            isRecording={true}
-          />
-          {isUserSupportingTeam && (
-            <TeamEmotionDistribution.ClickCount emotionGroupList={emotionGroupList} />
-          )}
-        </SectionContainer>
-    )
-  }
-
-  return <></>
-}
 
 const SectionContainer = ({ children }: { children: React.ReactNode }) => {
   return (
@@ -60,4 +15,58 @@ const SectionContainer = ({ children }: { children: React.ReactNode }) => {
       </div>
     </>
   )
+}
+
+export const EmotionDistribution = ({
+  matchRecordId,
+}: {
+  matchRecordId: number
+}) => {
+  const { isUserSupportingTeam, isDuringMatch, recordDetail } =
+    useGetRecordDetail({
+      matchRecordId,
+    })
+
+  if (!recordDetail) return <></>
+
+  const {
+    homeTeam,
+    awayTeam,
+    positiveEmotionPercent,
+    negativeEmotionPercent,
+    emotionGroupList,
+  } = recordDetail
+
+  if (isDuringMatch) {
+    return (
+      <>
+        <SectionContainer>
+          <TeamEmotionDistribution.Empty />
+        </SectionContainer>
+      </>
+    )
+  }
+
+  if (!isDuringMatch) {
+    return (
+      <SectionContainer>
+        <TeamsEmotionStat
+          homeTeamKey={homeTeam}
+          awayTeamKey={awayTeam}
+          homePositive={positiveEmotionPercent}
+          homeNegative={negativeEmotionPercent}
+          awayPositive={positiveEmotionPercent}
+          awayNegative={negativeEmotionPercent}
+          isRecording={true}
+        />
+        {isUserSupportingTeam && (
+          <TeamEmotionDistribution.ClickCount
+            emotionGroupList={emotionGroupList}
+          />
+        )}
+      </SectionContainer>
+    )
+  }
+
+  return <></>
 }
