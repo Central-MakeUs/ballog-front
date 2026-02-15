bee329c8dedeff811fcc88055ddaeb09cfd47d1f
kych0912 <kyt031522@gmail.com>
2026-02-15 22:08:50 +0900

    다크모드 전환 시 앱바 로고와 소셜 아이콘 색상 동기화

--- apps/web/src/assets/BallogAppBar.tsx
+++ apps/web/src/assets/BallogAppBar.tsx
@@ -1,12 +1,19 @@
 import { ColoredBallogLogo, WhiteBallogLogo } from '@ballog/asset/icons'
+import type { SVGProps } from 'react'
+import { useSyncExternalStore } from 'react'
 
-const BallogAppBar = () => {
-  return (
-    <>
-      <WhiteBallogLogo className="h-16 w-auto hidden dark:block" />
-      <ColoredBallogLogo className="h-16 w-auto block dark:hidden" />
-    </>
+import { cn } from '@/shared/lib/classnames'
+import { themeStore } from '@/shared/lib/theme'
+
+const BallogAppBar = (props: SVGProps<SVGSVGElement>) => {
+  const { className, ...rest } = props
+  const theme = useSyncExternalStore(
+    themeStore.subscribe,
+    themeStore.getSnapshot,
   )
+  const Logo = theme === 'dark' ? WhiteBallogLogo : ColoredBallogLogo
+
+  return <Logo className={cn('h-16 w-auto', className)} {...rest} />
 }
 
 export default BallogAppBar
