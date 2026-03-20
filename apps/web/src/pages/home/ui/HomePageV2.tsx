c00bcf491a892b3064408b7cba37c8e5de03bc67
kimyounghee425 <doctorsean0425@gmail.com>
2026-03-20 15:29:57 +0900

    💚 Build

--- apps/web/src/pages/home/ui/HomePageV2.tsx
+++ apps/web/src/pages/home/ui/HomePageV2.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { AppScreen } from '@stackflow/plugin-basic-ui'
 import { type ActivityComponentType } from '@stackflow/react'
 import { useQuery } from '@tanstack/react-query'
