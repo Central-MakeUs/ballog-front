712769d9e719b13828b761e8b7ac2426f1c674c2
kych0912 <kyt031522@gmail.com>
2026-05-14 21:13:41 +0900

    🔧 Chore : MSW mocking 비활성화
    bootstrap 시 startMocking 호출과 import를 제거하여
    모든 API 요청이 실제 백엔드로 향하도록 변경

--- apps/web/src/app/main.tsx
+++ apps/web/src/app/main.tsx
@@ -1,13 +1,10 @@
 import { createRoot } from 'react-dom/client'
 
 import '../shared/ui/global.css'
-import { startMocking } from '@/mocks/browser'
 
 import App from './App'
 
 function bootstrap() {
-  startMocking()
-
   createRoot(document.getElementById('root')!).render(
     <>
       <App />
